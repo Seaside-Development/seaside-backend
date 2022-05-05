9f541fd35f3b5f0fe0176dd36985c70b538621b7
RicePeas <ricepeas246@gmail.com>
2022-05-05 02:58:31 -0400

    Added more schemas

--- models/contractorModel.js
+++ models/contractorModel.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const  services = require('./servicesNodel').services;
 
-const jobRequestsSchema = mongoose.Schema({
+
+const contractorSchema = mongoose.Schema({
     _id: { type: mongoose.Schema.Types.ObjectId, required: true, unique: true, index: true }, // _id is required and unique
-    contractor: { type: String, required: true },
-    businessDescription: { type: string, required: true, default: '' },                         
+    contractorName: { type: String, required: true },
+    businessDescription: { type: String, required: true, default: '' },                         
     operatingLocations: [{
         type: String,
         enum: [
@@ -18,17 +20,37 @@ const jobRequestsSchema = mongoose.Schema({
             'St. John',
             'Christ Church',
             'St. Philip'
-        ]
+        ],
+        required:[true, 'Please select a parish of operations']
+    
 
-    }], 
-    ratings: [
-        {
-            completedJobs: { type: Number, required: true, default: 0 },
-            avgRating: { type: Number, default: 0, required:true },
-        }
-    ],
-    activeJobs: {type: mongoose.Schema.ObjectId},
-
+    }],
+    
+    services:[{ type: mongoose.Schema.Types.ObjectId, required: true, ref: services }],
+    ratings: {ratings, required: false, defualt: undefined}
 });
 
-module.exports = mongoose.model('JobRequests', jobRequestsSchema);
+
+const ratings=new mongoose.Schema({
+    completedJobs: { type: Number, required: true, default: 0 },
+    avgRating: { type: Number, default: undefined, required:true },
+})
+
+
+//THIS IS ACTING AS A POTENTIAL SUBSCHEMA AND MAY NOT BE NEEDED WITH IMPLEMNTATION
+const servicesSchema = mongoose.Schema({
+    _id: { type: mongoose.Schema.Types.ObjectId, required: true, unique: true, index: true }, // _id is required and unique
+    
+    services:[{
+        Plumbing: [{type: Array, enum:['installation', 'removal', 'repairs']}],
+        Gardening: [{type: Array, enum:['lawn maintainence', 'planting']}],
+        Construction: [{type: Array, enum:['masonry', 'design', 'construction', 'roofing', 'Land Surveor', 'painting and decorating', 'carpentry', 'insulation', 'tiling', 'glazier'] }],
+        Electrical: [{ type: Array, enum:['installtion','repairs']}]
+    }]
+    
+    
+});
+
+
+
+module.exports.contractors = mongoose.model('contractors', contractorSchema);
