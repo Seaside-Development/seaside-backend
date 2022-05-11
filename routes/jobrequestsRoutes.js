c9b2d42a066ddebbd2b029bab2d455682f6475e7
Nate Brate <62619983+natebrate@users.noreply.github.com>
2022-05-11 14:50:15 -0400

    update job routers

--- routes/jobrequestsRoutes.js
+++ routes/jobrequestsRoutes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const {getJobrequests, setJobrequest, updateJobrequest, deleteJobrequest} = require('../controllers/jobrequestsController');// Importing the serviceController
 const {protect} = require('../middleware/authMiddleware');// Importing the authMiddleware
 
-router.route('/').get(getJobrequests).post(protect, setJobrequest);
-router.route('/:id').put(protect, updateJobrequest).delete(protect, deleteJobrequest);
+router.get('/', getJobrequests);
+router.post('/createjob', setJobrequest);
+router.put('/updatejob/:id', updateJobrequest);
+router.delete('/removejob/:id', deleteJobrequest);
 
 module.exports = router;
