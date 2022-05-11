5f45160e8201c17f88752725fa9e0839be50a19c
RicePeas <ricepeas246@gmail.com>
2022-05-11 17:33:55 -0400

    Gerenal Fixes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const {getUsers, loginUser, registerUser, updateUser, deleteUser, getMe} = require('../controllers/userController.js');// Importing the serviceController
 const {protect} = require('../middleware/authMiddleware.js');// Importing the authMiddleware
 
-router.get('/', getUsers);
+router.get('/', prtoect, getUsers);
 router.post('/login', loginUser);
-router.post('/register', protect,registerUser);
+router.post('/register', registerUser);
 router.put('/update/:id', protect,updateUser)
 router.delete('/remove/:id', protect,deleteUser);
 router.get('/me', protect, getMe);
