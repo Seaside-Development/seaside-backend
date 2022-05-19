2f8cda48f9b163037a90008c888872873150bfe6
RicePeas <ricepeas246@gmail.com>
2022-05-18 20:04:54 -0400

    Added CORS

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
+const cors=require('cors')
 
 connectDB();
 
@@ -11,6 +12,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//Enable CORS
+app.use('/', cors())
 
 //api pathway
 app.use('/api/users', require('./routes/userRoutes'));
