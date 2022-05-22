347c3b4c45ec720473a275ef544b7c060e16ee82
Nate Brate <62619983+natebrate@users.noreply.github.com>
2022-05-22 15:26:00 -0400

    added search route

--- routes/ContractorsRoutes.js
+++ routes/ContractorsRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {getContractors, registerContractor, updateContractors, deleteContractors, getMe} = require('../controllers/contractorController');// Importing the serviceController
+const {getContractors, registerContractor, updateContractors, deleteContractors, getMe, getContractorsBySearch} = require('../controllers/contractorController');// Importing the serviceController
 const {protect} = require('../middleware/authMiddleware');// Importing the authMiddleware
 
 router.get('/', protect,getContractors);
@@ -8,5 +8,6 @@ router.post('/regcontractor', protect, registerContractor);
 router.put('/update/:id', protect, updateContractors);
 router.delete('/remove/:id' , protect, deleteContractors);
 router.get('/me', protect, getMe);
+router.get('/search', getContractorsBySearch);
 
 module.exports = router;
