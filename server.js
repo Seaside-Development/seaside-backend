b27c4fc2022f8c986571e977d633447e189a901c
khareenp <khareenp1@gmail.com>
2022-05-28 13:47:24 -0400

    update

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 //imports
-const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv').config();
-const ejs = require('ejs');
-const path = require('path');
-const {errorHandler} = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db');
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const ejs = require("ejs");
+const path = require("path");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
-const cors=require('cors')
+const cors = require("cors");
 
 connectDB();
 
@@ -17,38 +17,37 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //Enable CORS
-app.use('/', cors())
+app.use("/", cors());
 
 //static files
-app.use(express.static('public'));
-app.use('/css', express.static(__dirname + 'frontend/public/css'));
-app.use('/css', express.static(__dirname + 'frontend/public/js'));
-app.use('/css', express.static(__dirname + 'frontend/public/attachments'));
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "frontend/public/css"));
+app.use("/css", express.static(__dirname + "frontend/public/js"));
+app.use("/css", express.static(__dirname + "frontend/public/attachments"));
 
 //set views
-app.set('views', './views');
-app.set('view engine', 'ejs');
+app.set("views", "./views");
+app.set("view engine", "ejs");
 //display the ejs files
-app.get('', (req, res) => {
-    res.render('index')
-})  //end of app.get
-app.get('/contractorform', (req, res) => {
-    res.render('contractorform')
-})  //end of app.get
+app.get("", (req, res) => {
+  res.render("index");
+}); //end of app.get
+app.get("/user-registration-form", (req, res) => {
+  res.render("user-registration-form");
+}); //end of app.get
 
 //api pathways
-app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/jobrequests', require('./routes/jobrequestsRoutes'));
-app.use('/api/contractor', require('./routes/ContractorsRoutes'));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/jobrequests", require("./routes/jobrequestsRoutes"));
+app.use("/api/contractor", require("./routes/ContractorsRoutes"));
 //pathway to the error
 app.use(errorHandler);
 
-
 // check that app is running on production mode
-if (process.env.NODE_ENV === 'production') {
-    //forces https on the website
-    app.use(compression());
-    app.use(enforce.HTTPS({ trustProtoHeader: true }));
+if (process.env.NODE_ENV === "production") {
+  //forces https on the website
+  app.use(compression());
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
 }
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
