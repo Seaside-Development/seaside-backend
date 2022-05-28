6df6cc5a3eee2497555bda000cbdc7c522165b32
Nate Brate <62619983+natebrate@users.noreply.github.com>
2022-05-28 15:58:53 -0400

    added layout for ejs pages

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 //imports
+const { errorHandler } = require("./middleware/errorMiddleware");
+const expressLayouts = require("express-ejs-layouts");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
 const express = require("express");
 const colors = require("colors");
-const dotenv = require("dotenv").config();
-const ejs = require("ejs");
 const path = require("path");
-const { errorHandler } = require("./middleware/errorMiddleware");
-const connectDB = require("./config/db");
-const port = process.env.PORT || 5000;
 const cors = require("cors");
+const ejs = require("ejs");
 
-connectDB();
+const port = process.env.PORT || 5000;
+
+connectDB(); // connect to database function
 
 //enable express middleware
 const app = express();
@@ -26,22 +28,19 @@ app.use("/css", express.static(__dirname + "frontend/public/js"));
 app.use("/css", express.static(__dirname + "frontend/public/attachments"));
 
 //set views
+app.use(expressLayouts); // allows us to use ejs layouts
 app.set("views", "./views");
 app.set("view engine", "ejs");
-//display the ejs files
-app.get("", (req, res) => {
-  res.render("index");
-}); //end of app.get
-app.get("/user-registration-form", (req, res) => {
-  res.render("user-registration-form");
-}); //end of app.get
+
+//render pages
+app.get("", (req, res) => { res.render("index");}); //display the index page
+app.get("/user-registration-form", (req, res) => {res.render("user-registration-form");}); //end of app.get
 
 //api pathways
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/jobrequests", require("./routes/jobrequestsRoutes"));
 app.use("/api/contractor", require("./routes/ContractorsRoutes"));
-//pathway to the error
-app.use(errorHandler);
+app.use(errorHandler); //pathway to the error handler
 
 // check that app is running on production mode
 if (process.env.NODE_ENV === "production") {
@@ -50,4 +49,5 @@ if (process.env.NODE_ENV === "production") {
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
 }
 
+//check for the port
 app.listen(port, () => console.log(`Server started on port ${port}`));
